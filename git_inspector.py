836df128dc49a1e718645b6dcf791e32e70f2aa0
Yanik Söltzer <23218364+yaniksoeltzer@users.noreply.github.com>
2019-08-30 15:24:55 +0200

    spelling

--- git_inspector.py
+++ git_inspector.py
@@ -6,7 +6,7 @@ from git_inspector import inspect_all
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('paths', metavar='paths', nargs='*', default='.',
+parser.add_argument('paths', metavar='path', nargs='*', default='.',
                     help='directory paths in which to look for git modules')
 args = vars(parser.parse_args())
 absolute_paths = [os.path.abspath(x) for x in args['paths']]
