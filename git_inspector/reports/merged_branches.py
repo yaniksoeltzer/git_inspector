b1a045bca3aab19e7f525a59dedc04d00eea6028
Yanik Söltzer <23218364+yaniksoeltzer@users.noreply.github.com>
2019-08-30 15:43:29 +0200

    pep8

--- git_inspector/reports/merged_branches.py
+++ git_inspector/reports/merged_branches.py
@@ -2,20 +2,6 @@ from git_inspector.common import *
 from git_inspector.reports.git_report import GitReport, GIT_REPORT_LEVEL_HINT
 
 
-def get_merged_heads(repo):
-    merged_heads = []
-    master = get_master_branch(repo)
-    if master:
-        non_masters = get_non_master_branches(repo)
-        for non_master in non_masters:
-            depth = is_ancestors_of(ancestor=non_master.commit, commit=master.commit)
-            if not depth:
-                pass
-            else:# depth >= 0:
-                merged_heads.append(non_master)
-    return merged_heads
-
-
 def get_merged_branches_report(repos):
     merged_heads = []
     for repo in repos:
@@ -28,3 +14,17 @@ def get_merged_branches_report(repos):
         [],
         merged_heads)
     return merged_branches_report
+
+
+def get_merged_heads(repo):
+    merged_heads = []
+    master = get_master_branch(repo)
+    if master:
+        non_masters = get_non_master_branches(repo)
+        for non_master in non_masters:
+            depth = is_ancestors_of(ancestor=non_master.commit, commit=master.commit)
+            if not depth:
+                pass
+            else:# depth >= 0:
+                merged_heads.append(non_master)
+    return merged_heads
--- git_inspector/reports/unpushed.py
+++ git_inspector/reports/unpushed.py
@@ -4,6 +4,21 @@ from git_inspector.common import get_tracked_heads, is_ancestors_of
 from git_inspector.reports.git_report import GitReport, GIT_REPORT_LEVEL_WARNING
 
 
+def get_unpushed_branches_report(repos):
+    unpushed_heads = []
+    for repo in repos:
+        repo_merged_heads = get_unpushed_branches(repo)
+        unpushed_heads.extend(repo_merged_heads)
+    report = GitReport(
+        'unpushed',
+        'unpushed branches',
+        GIT_REPORT_LEVEL_WARNING,
+        [],
+        unpushed_heads
+    )
+    return report
+
+
 def get_unpushed_branches(repo:Repo):
     unpushed_heads = []
     tracked_heads = get_tracked_heads(repo)
@@ -19,17 +34,3 @@ def get_unpushed_branches(repo:Repo):
             logging.warning(f"Error accessing {repo.working_dir}")
     return unpushed_heads
 
-
-def get_unpushed_branches_report(repos):
-    unpushed_heads = []
-    for repo in repos:
-        repo_merged_heads = get_unpushed_branches(repo)
-        unpushed_heads.extend(repo_merged_heads)
-    report = GitReport(
-        'unpushed',
-        'unpushed branches',
-        GIT_REPORT_LEVEL_WARNING,
-        [],
-        unpushed_heads
-    )
-    return report
