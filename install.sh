4a5b3bad31518538f8122fc2bb7abf7dfb483cb1
Yanik Söltzer <23218364+yaniksoeltzer@users.noreply.github.com>
2020-06-08 14:08:51 +0200

    close #13

--- install.sh
+++ install.sh
@@ -1,22 +1,52 @@
 #!/usr/bin/env bash
 set -e
 
-
 INSTALL_DIR='/opt/git_inspector'
+EXECUTABLE_LINK='/usr/local/bin/git_inspector'
+CURRENT_PATH=$(realpath $(dirname ${BASH_SOURCE[0]}))
 
 
-if [[ -d ${INSTALL_DIR} ]]
+if [ ${CURRENT_PATH} == ${INSTALL_DIR} ]
 then
-    echo "git_inspector is already installed"
-    exit 1
+   echo -e "\e[33malready in ${INSTALL_DIR}, skip copying to ${INSTALL_DIR}"
+   # expect repository is already copied to ${INSTALL_DIR}
+   # skip copying to ${INSTALL_DIR}
+else
+    if [[ -d ${INSTALL_DIR} ]]
+    then
+	echo -e "\e[41mFolder ${INSTALL_DIR} already exists\e[0m"
+	echo -n "update git_inspector? [y/n]"
+	read -n 1 SHOULD_UPDATE
+	echo ""
+	if [ ${SHOULD_UPDATE} == "y" ]
+	then
+	    ./un_install.sh
+	else
+	    echo -e "\e[91mabort installation"
+	    exit 1
+	fi
+    fi
+    # copy current directory to INSTALL_DIR
+    sudo mkdir ${INSTALL_DIR}
+    sudo cp -r . ${INSTALL_DIR}
 fi
 
-# copy and install venv
-sudo mkdir ${INSTALL_DIR}
-sudo cp -r . ${INSTALL_DIR}
+# install python requirements
 cd ${INSTALL_DIR}
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt > /dev/null
 
 
 # link path
+if [[ -f ${EXECUTABLE_LINK} ]]
+then
+    sudo rm ${EXECUTABLE_LINK}
+fi
 sudo ln -s ${INSTALL_DIR}/git_inspector.py /usr/local/bin/git_inspector
+
+echo -e "\e[92minstalled git_inspector\e[39m"
+
+if [ ${CURRENT_PATH} != ${INSTALL_DIR} ]
+then
+    echo "you can remove this directory"
+fi
+
--- un_install.sh
+++ un_install.sh
@@ -2,6 +2,7 @@
 set -e
 
 INSTALL_DIR='/opt/git_inspector'
+CURRENT_PATH=$(realpath $(dirname ${BASH_SOURCE[0]}))
 
 if [[ ! -d ${INSTALL_DIR} ]]
 then
@@ -9,9 +10,11 @@ then
     exit 1
 fi
 
-
 # copy and install venv
-rm -rf ${INSTALL_DIR}
+sudo rm -rf ${INSTALL_DIR}
 
 # link path
 sudo rm /usr/local/bin/git_inspector
+
+echo -e "\e[92mun_installed git_inspector\e[39m"
+
