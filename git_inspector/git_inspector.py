24882e9df08ddff8764b155cccf343316eb2ceda
Yanik SÃ¶ltzer <yanik.soeltzer@rwth-aachen.de>
2021-08-20 21:21:33 +0200

    cleanup

--- git_inspector/git_inspector.py
+++ git_inspector/git_inspector.py
@@ -4,12 +4,7 @@ from git_inspector.reports import get_git_reports
 
 
 def inspect_all(paths):
-    repos = get_git_repos(paths)
+    repo_file_names = find_git_repositories(search_paths=paths)
+    repos = list(map(Repo, repo_file_names))
     reports = get_git_reports(repos)
     return repos, reports
-
-
-def get_git_repos(paths):
-    repo_file_names = find_git_repositories(search_paths=paths)
-    repos = map(Repo, repo_file_names)
-    return list(repos)
