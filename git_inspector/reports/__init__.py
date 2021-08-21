5188fd36d7508a1b81ac762e5bb52036b1934983
Yanik SÃ¶ltzer <yanik.soeltzer@rwth-aachen.de>
2021-08-21 19:14:07 +0200

    update untracked reports

--- git_inspector/reports/__init__.py
+++ git_inspector/reports/__init__.py
@@ -1,7 +1,8 @@
-from git_inspector.reports.dirty import get_dirty_report
-from git_inspector.reports.merged import get_merged_report
-from git_inspector.reports.unpushed import get_unpushed_report
-from git_inspector.reports.untracked import get_untracked_report
+from .report import *
+from .dirty import get_dirty_report
+from .merged import get_merged_report
+from .unpushed import get_unpushed_report
+from .untracked import get_untracked_report
 
 
 def get_reports(repos):
@@ -9,6 +10,6 @@ def get_reports(repos):
     for repo in repos:
         reports.append(get_dirty_report(repo))
         reports.append(get_merged_report(repo))
-        reports.append(get_untracked_report(repo))
         reports.append(get_unpushed_report(repo))
+        reports.append(get_untracked_report(repo))
     return [r for r in reports if r is not None]
--- git_inspector/reports/untracked.py
+++ git_inspector/reports/untracked.py
@@ -1,22 +1,21 @@
-import logging
 from git import Repo
-from git_inspector.common import get_tracked_heads, is_ancestors_of, get_untracked_heads
-from git_inspector.reports.git_report import GitReport, GIT_REPORT_LEVEL_WARNING
+from git_inspector.common import get_untracked_heads
+
+from .report import Report, ReportType, GIT_REPORT_LEVEL_WARNING
+
+untracked_report = ReportType(
+    'untracked',
+    'untracked branches',
+    GIT_REPORT_LEVEL_WARNING
+)
 
 
-def get_untracked_report(repos):
-    untracked_heads = []
-    for repo in repos:
-        repo_untracked_heads = get_untracked_branches(repo)
-        untracked_heads.extend(repo_untracked_heads)
-    report = GitReport(
-        'untracked',
-        'untracked branches',
-        GIT_REPORT_LEVEL_WARNING,
-        [],
-        untracked_heads
-    )
-    return report
+def get_untracked_report(repo):
+    untracked_branches = get_untracked_branches(repo)
+    if len(untracked_branches) > 0:
+        return Report(repo, untracked_branches, untracked_report)
+    else:
+        return None
 
 
 def get_untracked_branches(repo: Repo):
