0e885b5e9d70d6ff851cb3376a7e0c3355fed25d
Yanik SÃ¶ltzer <yanik.soeltzer@rwth-aachen.de>
2021-08-22 18:32:58 +0200

    add html example output

--- documentation/create_sample_output.py
+++ documentation/create_sample_output.py
@@ -1,7 +1,15 @@
 from test_setup import test_setup
 from git_inspector import inspect
+from ansi2html import Ansi2HTMLConverter
 
+HTML_FILE = "example_output.html"
 
 if __name__ == '__main__':
+    conv = Ansi2HTMLConverter()
     with test_setup() as directory:
-        print(inspect([directory]))
+        output = inspect([directory])
+        print(output)
+        html = conv.convert(output)
+        with open(HTML_FILE, "w+") as f:
+            f.write(html)
+
