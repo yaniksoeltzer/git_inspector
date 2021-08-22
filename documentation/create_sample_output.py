b88dd289b1cd4fe273d74004b2874e4aa52aa6c3
Yanik SÃ¶ltzer <yanik.soeltzer@rwth-aachen.de>
2021-08-22 19:13:31 +0200

    use png example output

--- documentation/create_sample_output.py
+++ documentation/create_sample_output.py
@@ -1,26 +1,25 @@
-from tempfile import NamedTemporaryFile
 
-import pdfkit
 
 from test_setup import test_setup
 from git_inspector import inspect
 from ansi2html import Ansi2HTMLConverter
+from html2image import Html2Image
 
-PDF_FILE = "example_output.pdf"
-options = {
-    'page-height': '5cm',
-    'page-width': '15cm',
-    'margin-top': '0',
-    'margin-right': '0',
-    'margin-bottom': '0',
-    'margin-left': '0',
-}
+PNG_FILE = "example_output.png"
+BASH_PROMPT = "user@device:~$ git_inspector\n"
 
 if __name__ == '__main__':
-    conv = Ansi2HTMLConverter()
+    conv = Ansi2HTMLConverter(dark_bg=False,)
     with test_setup() as directory:
         output = inspect([directory])
+        output = BASH_PROMPT + output
         print(output)
         html = conv.convert(output)
-        pdfkit.from_string(html, PDF_FILE, options=options)
+        hti = Html2Image()
+        size = 3.5
+        hti.screenshot(
+            html_str=html,
+            save_as=PNG_FILE,
+            size=(int(170*size), int(50*size)),
+        )
 
