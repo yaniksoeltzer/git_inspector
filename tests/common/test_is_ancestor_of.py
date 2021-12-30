9db096923eac9e5eef73c92b8dd12103c976c460
Yanik Söltzer <23218364+yaniksoeltzer@users.noreply.github.com>
2021-12-30 22:19:07 +0100

    add a test

--- tests/common/test_is_ancestor_of.py
+++ tests/common/test_is_ancestor_of.py
@@ -1,6 +1,8 @@
 from git import Repo
 from git_inspector.common import is_ancestors_of
 
+from tests.testutils import add_ahead_branch
+
 
 def test_same_commit(repo: Repo):
     assert is_ancestors_of(
@@ -24,3 +26,15 @@ def test_distant_ancestor(repo: Repo):
     assert is_ancestors_of(
         ancestor=repo.commit("HEAD~2"),
         commit=repo.commit("HEAD"))
+
+
+def test_ahead_branch(repo: Repo):
+    ahead_branch = add_ahead_branch(repo)
+    assert is_ancestors_of(
+        ancestor=repo.commit("HEAD"),
+        commit=ahead_branch.commit
+    )
+    assert not is_ancestors_of(
+        ancestor=ahead_branch.commit,
+        commit=repo.commit("HEAD")
+    )
