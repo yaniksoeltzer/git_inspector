628d2d7d407f789887649467c01d6cd5bfd87560
Yanik Söltzer <23218364+yaniksoeltzer@users.noreply.github.com>
2022-01-07 19:06:44 +0100

    update README.md

--- documentation/generate_example_output.py
+++ documentation/generate_example_output.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-from git_inspector.reports import Report
+from git_inspector.reports import Report, GIT_REPORT_LEVEL_HINT
 from git_inspector.reports.dirty import dirty_report
 from git_inspector.reports.merged import merged_report
 from git_inspector.reports.unpushed import unpushed_report
@@ -10,14 +10,14 @@ from git_inspector.cli.continuous_git_reporter import ContinuousGitReporter
 
 if __name__ == '__main__':
     reports = [
-        Report("~/my_dirty_repo", None, None, dirty_report),
-        Report("~/my_dirty_repo_2", None, None, dirty_report),
-        Report("~/forgot_to_push", ["master"], None, unpushed_report),
-        Report("~/forgot_to_push", None, ["origin"], broken_remote_report),
-        Report("~/repo_with_old_branch", ["old_branch"], None, merged_report),
-        Report("~/repo_w_local_only_branch", ["local_only"], None, untracked_report),
+        Report("~/my_dirty_repo", {}, dirty_report),
+        Report("~/my_dirty_repo_2", {}, dirty_report),
+        Report("~/forgot_to_push", {"branch": "master"}, unpushed_report),
+        Report("~/forgot_to_push", {"remote": "origin"}, broken_remote_report),
+        Report("~/repo_with_old_branch", {"branch": "old_branch"}, merged_report),
+        Report("~/repo_w_local_only_branch", {"branch": "local_only"}, untracked_report),
     ]
-    with ContinuousGitReporter() as reporter:
+    with ContinuousGitReporter(report_level=GIT_REPORT_LEVEL_HINT) as reporter:
         for report in reports:
             sleep(0.5)
             reporter.add_repo(None)
