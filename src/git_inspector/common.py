a98ca81bcb8fb1fae3ad00ca37c40f7a516918e5
Yanik Söltzer <23218364+yaniksoeltzer@users.noreply.github.com>
2022-01-23 23:43:04 +0100

    use while loop instead of recursion
    this solves the max recursion problem for the `test_very_distant_ancestor` test in `test_is_ancestor.py`

--- src/git_inspector/common.py
+++ src/git_inspector/common.py
@@ -41,30 +41,21 @@ def is_ancestors_of(ancestor: Commit, commit: Commit):
     return depth_search_commit([commit], [], ancestor) is not None
 
 
-def depth_search_commit(fringe: iter, visited: list, goal_node: Commit, cur_dept=0):
-    if len(fringe) == 0:
-        return None
-    if cur_dept >= 100:
-        logging.debug(f"max depth reached for is_ancestors_of of {goal_node.repo.working_dir}")
-        return None
-    if goal_node in fringe:
-        return 0
-    new_fringe = []
-    for fring in fringe:
-        f_parents = fring.parents
-        new_fringe.extend(f_parents)
-    new_fringe = [x for x in new_fringe if x not in visited]
-    visited += fringe
-    try:
-        dept = depth_search_commit(new_fringe, visited, goal_node, cur_dept + 1)
-    except ValueError as e:
-        # A value error like the following is risen if the remote can not be resolved any further
-        # ValueError: SHA b'7692881d2a61a4ba47eeef5d7827c0d2cb896def' could not be resolved, git returned:
-        # b'7692881d2a61a4ba47eeef5d7827c0d2cb896def missing
-        raise CommitResolveError(e)
-    if dept is not None:
-        return dept + 1
-    return None
+def depth_search_commit(fringe: iter, visited: list, goal_node: Commit):
+    cur_dept = 0
+    while True:
+        if len(fringe) == 0:
+            return None
+        if goal_node in fringe:
+            return cur_dept
+        new_fringe = []
+        for fring in fringe:
+            f_parents = fring.parents
+            new_fringe.extend(f_parents)
+        new_fringe = [x for x in new_fringe if x not in visited]
+        visited += fringe
+        fringe = new_fringe
+        cur_dept += 1
 
 
 def remote_is_gone(remote: Remote):
